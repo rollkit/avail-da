8815ef78381c49c9492c63efb16b4851e7f6f044
chandiniv1 <velamalachandini9@gmail.com>
2023-12-21 11:21:35 +0530

    Revert "fix lints"
    This reverts commit 44ee24a95c03640d69ffb9e77a10995efb47a303.

--- avail.go
+++ avail.go
@@ -7,12 +7,11 @@ import (
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"github.com/rollkit/go-da"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
-
-	"github.com/rollkit/go-da"
 )
 
 type SubmitRequest struct {
